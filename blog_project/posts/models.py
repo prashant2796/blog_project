c6905666f41bced9823cf33cd07d5764a024b2d0
Prashant Pandey <prashantpandey071@gmail.com>
2017-08-10 01:54:48 +0530

    implemented img responsive and render markdown

--- blog_project/posts/models.py
+++ blog_project/posts/models.py
@@ -4,9 +4,12 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save
+from django.utils.safestring import mark_safe
 from django.utils import timezone
 
 from django.utils.text import slugify
+
+from markdown_deux import markdown
 # Create your models here.
 # MVC MODEL VIEW CONTROLLER
 
@@ -17,14 +20,14 @@ from django.utils.text import slugify
 class PostQuerySet(models.query.QuerySet):
     def not_draft(self):
         return self.filter(draft=False)
-    
+
     def published(self):
         return self.filter(publish__lte=timezone.now()).not_draft()
 
 class PostManager(models.Manager):
     def get_queryset(self, *args, **kwargs):
         return PostQuerySet(self.model, using=self._db)
-            
+
     def active(self, *args, **kwargs):
         # Post.objects.all() = super(PostManager, self).all()
         return self.get_queryset().published()
@@ -37,7 +40,7 @@ def upload_location(instance, filename):
     new_id = PostModel.objects.order_by("id").last().id + 1
     """
     instance.__class__ gets the model Post. We must use this method because the model is defined below.
-    Then create a queryset ordered by the "id"s of each object, 
+    Then create a queryset ordered by the "id"s of each object,
     Then we get the last object in the queryset with `.last()`
     Which will give us the most recently created Model instance
     We add 1 to it, so we get what should be the same id as the the post we are creating.
@@ -48,10 +51,10 @@ class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
-    image = models.ImageField(upload_to=upload_location, 
-            null=True, 
-            blank=True, 
-            width_field="width_field", 
+    image = models.ImageField(upload_to=upload_location,
+            null=True,
+            blank=True,
+            width_field="width_field",
             height_field="height_field")
     height_field = models.IntegerField(default=0)
     width_field = models.IntegerField(default=0)
@@ -74,7 +77,10 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-timestamp", "-updated"]
-       
+    def get_markdown(self):
+        content = self.content
+        return mark_safe(markdown(content))
+
 #    @property
 #    def title(self):
 #        return "Title"
@@ -106,13 +112,3 @@ def pre_save_post_receiver(sender, instance, *args, **kwargs):
 
 
 pre_save.connect(pre_save_post_receiver, sender=Post)
-
-
-
-
-
-
-
-
-
-
