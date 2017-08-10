778a993b9246539a5e344affc145519419996341
Prashant Pandey <prashantpandey071@gmail.com>
2017-08-11 01:51:55 +0530

    implemented django generic foreign keys

--- blog_project/posts/views.py
+++ blog_project/posts/views.py
@@ -10,11 +10,12 @@ except:
 
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.contenttypes.models import ContentType
 from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-
+from comments.models import Comments
 from .forms import PostForm
 from .models import Post
 
@@ -67,10 +68,14 @@ def post_detail(request, slug=None):
 		if not request.user.is_staff or not request.user.is_superuser:
 			raise Http404
 	share_string = quote_plus(instance.content)
+    content_type = ContentType.objects.get_for_model(Post)
+    obj_id = instance.id
+    comments = Comment.objects.filter(content_type=content_type,object_id=obj_id)
 	context = {
 		"title": instance.title,
 		"instance": instance,
 		"share_string": share_string,
+        "comments": comments,
 	}
 	return render(request, "post_detail.html", context)
 
